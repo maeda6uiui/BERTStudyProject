9435c5c181daff23071188914cde823703258ef3
maeda6uiui <63230315+maeda6uiui@users.noreply.github.com>
2020-05-05 07:20:33 -0700

    ファイルを追加

---
+++ bert.py
@@ -0,0 +1,69 @@
+import numpy as np
+import sys
+import torch
+from transformers import BertTokenizer,BertForMaskedLM,BertConfig
+from pyknp import Juman
+
+juman=None
+
+config=None
+model=None
+bert_tokenizer=None
+
+def Setup():
+    global juman
+    juman=Juman(jumanpp=True)
+
+    global config
+    global model
+    global bert_tokenizer
+    config=BertConfig.from_json_file(
+    "/home/maeda/Documents/Laboratory/Japanese_L-24_H-1024_A-16_E-30_BPE_WWM/bert_config.json")
+    model=BertForMaskedLM.from_pretrained(
+        "/home/maeda/Documents/Laboratory/Japanese_L-24_H-1024_A-16_E-30_BPE_WWM/pytorch_model.bin",config=config)
+    bert_tokenizer=BertTokenizer(
+        "/home/maeda/Documents/Laboratory/Japanese_L-24_H-1024_A-16_E-30_BPE_WWM/vocab.txt",
+        do_lower_case=False,do_basic_tokenize=False)
+
+    model.eval()
+
+def Predict(text):
+    result=juman.analysis(text)
+
+    mrph_list=result.mrph_list()
+    masked_index=-1
+
+    for i,mrph in enumerate(mrph_list):
+        if mrph.midasi=='*':
+            masked_index=i+1
+            break
+
+    if masked_index<0:
+        print("Error: マスクされている形態素がありません。")
+        return
+
+    tokenized_text=[mrph.midasi for mrph in mrph_list]
+    tokenized_text.insert(0,"[CLS]")
+    tokenized_text.append("[SEP]")
+    tokenized_text[masked_index]="[MASK]"
+
+    tokens=bert_tokenizer.convert_tokens_to_ids(tokenized_text)
+    tokens_tensor=torch.tensor([tokens])
+
+    tokens_tensor=tokens_tensor.to("cpu")
+    model.to("cpu")
+
+    with torch.no_grad():
+        outputs=model(tokens_tensor)
+        predictions=outputs[0]
+
+    _,predicted_indexes=torch.topk(predictions[0,masked_index],k=5)
+    predicted_tokens=bert_tokenizer.convert_ids_to_tokens(predicted_indexes.tolist())
+    print(predicted_tokens)
+
+if __name__=="__main__":
+    Setup()
+
+    text=sys.stdin.readline()
+    text=text.rstrip()
+    Predict(text)
